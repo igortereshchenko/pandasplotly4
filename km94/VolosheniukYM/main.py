8f7d61f4ce54172186983836a6839c5bab3de896
Yura <volosheniukyura@gmail.com>
2019-12-20 17:02:48 +0200

    I added description and main.py with 2 tasks

---
+++ km94/VolosheniukYM/main.py
@@ -0,0 +1,67 @@
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="keys.json"
+
+import pandas as pd
+from bq_helper import BigQueryHelper
+import plotly.graph_objs as go
+from plotly.offline import plot
+
+bq_assistant = BigQueryHelper('bigquery-public-data', 'census_bureau_international')
+
+
+QUERY = """
+        SELECT `year`, `infant_mortality`, `country_name`, `life_expectancy`
+        FROM `bigquery-public-data.census_bureau_international.mortality_life_expectancy`
+        LIMIT 10000
+        """
+
+
+df = bq_assistant.query_to_pandas(QUERY)
+
+Infant_mortality = df[df.infant_mortality >= 150]
+
+trace1 = go.Bar(
+                x=Infant_mortality.year,
+                y=Infant_mortality.infant_mortality,
+                name="Infant_mortality",
+                marker=dict(color = 'rgba(255, 174, 255, 0.5)',
+                             line=dict(color='rgb(0,0,0)',width=1.5)),
+                text=Infant_mortality.country_name
+                )
+
+
+Armenia = df[df.country_name == 'Armenia']
+
+trace2 = go.Scatter(
+                    x=Armenia.year,
+                    y=Armenia.life_expectancy,
+                    mode="lines",
+                    name="Life expectancy",
+                    marker=dict(color = 'rgba(16, 112, 2, 0.8)'),
+                    text=Armenia.country_name)
+
+
+
+trace3 = go.Pie(
+
+                    )
+
+
+data = [trace1]
+
+layout = dict(
+              title='',
+              xaxis= dict(title= ''),
+              yaxis=dict(title=''),
+             )
+fig = dict(data = [trace1], layout = layout)
+plot(fig)
+
+layout = dict(
+              title='',
+              xaxis= dict(title= ''),
+              yaxis=dict(title=''),
+             )
+fig = go.Figure(data=[trace2], layout=layout)
+plot(fig)
+
