5a22ccb5d1e134bab435b28fe8ecfb9904ead880
Romanov-Niko <kolya.romanov8@gmail.com>
2019-12-20 16:25:02 +0200

    bar

---
+++ km94/LashtabegAV/main.py.py
@@ -0,0 +1,21 @@
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "keys.json"
+
+import pandas as pd
+from bq_helper import BigQueryHelper
+import plotly.graph_objs as go
+from plotly.offline import plot
+
+bq_assistant = BigQueryHelper('bigquerty-public-data', 'census_bureau_international')
+
+QWERY = """
+SELECT country_name, year, sex, max_age FROM 'bigquerty-public-data.census_bureau_international.midyear_population_age_sex'
+LIMIT 10000
+"""
+
+df = bq_assistant.query_to_pandas(QWERY)
+
+Aruba = df[df.country_name == 'Aruba']
+bar = plot.bar(Aruba, x = 'year', y = 'max_age',
+               labels = {'year': 'Year', 'max_age': 'Max age'}, title = 'Aruba')
+bar.show()
