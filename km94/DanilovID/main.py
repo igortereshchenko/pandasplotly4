9095c35da5da9aae81aade4bede9ecee98d5aa63
Student <tereshchenko.igor@gmail.com>
2019-12-20 16:25:22 +0200

    bar

---
+++ km94/DanilovID/main.py
@@ -0,0 +1,24 @@
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="keys.json"
+
+import pandas as pd
+from bq_helper import BigQueryHelper
+import plotly.graph_objs as go
+from plotly.offline import plot
+import plotly.express as pe
+
+bq_assistant = BigQueryHelper("census-bureau-international","birth_death_growth_rates")
+
+
+QUERY = """
+        SELECT  country_code, year, crude_birth_rate, crude_death_rate FROM `bigquery-public-data.census_bureau_international.birth_death_growth_rates`
+        LIMIT 10000
+        """
+
+
+df = bq_assistant.query_to_pandas(QUERY)
+
+
+bar = pe.bar(df[df.crude_birth_rate>=30], x= "year", y="crude_birth_rate", labels = {"year": "Year","crude_birth_rate": "crude birth rate"}, title = "crude birth rate>=30")
+bar.show()
+
