ec21b639e3cb52535fb0f3bd430176489c4d4830
Your Name <you@example.com>
2019-12-20 17:10:08 +0200

    Pie

--- km94/amelin/main.py
+++ km94/amelin/main.py
@@ -19,13 +19,10 @@ QUERY = """
 df = bq_assistant.query_to_pandas(QUERY)
 
 
-df1 = df[df['country_name']=='Aruba']
-x = df1['year']
-y = df1['midyear_population']
 
-trace1 = go.Scatter( x=x,
-                     y=y,
-                     mode = "lines")
-
-fig1 = dict(data = [trace1])
-plot(fig1)
+df2 = df[df['year']==1981]
+labels = df2['country_name']
+values = df2['midyear_population']
+trace2 = go.Pie(labels = labels, values = values)
+fig = go.Figure(trace2)
+plot(fig)
