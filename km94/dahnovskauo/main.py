1df5370c210bb240b6ad899cf2993cf98a2ce8f9
UlianaDahnovska <55241559+UlianaDahnovska@users.noreply.github.com>
2019-12-20 17:01:31 +0200

    bar

---
+++ km94/dahnovskauo/main.py
@@ -0,0 +1,32 @@
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="keys.json"
+
+import pandas as pd
+from bq_helper import BigQueryHelper
+from google.cloud import bigquery
+import plotly.graph_objs as go
+from plotly.offline import plot
+
+client = bigquery.Client()
+bq_assistant = BigQueryHelper('bigquery-public-data', 'census_bureau_international')
+
+QUERY = """
+        SELECT `country_name`, `year`, `rate_natural_increase`, `growth_rate` 
+        FROM `bigquery-public-data.census_bureau_international.birth_death_growth_rates`
+        LIMIT 10000
+        """
+
+df = bq_assistant.query_to_pandas(QUERY)
+
+
+
+
+trace3 = go.Bar(x = df['country_name'], y = df['growth_rate'])
+layout3 = go.Layout(
+    title='Швидкість росту населення',
+    xaxis = dict(title='Country'),
+    yaxis = dict(title='Grown Rate')
+)
+
+fig1 = go.Figure(data=[trace3], layout=layout3)
+plot(fig1)
