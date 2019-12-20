3de45ff65757c5547be420d399f53b53641ab176
Your Name <you@example.com>
2019-12-20 15:58:07 +0200

    bar

--- km94/tereshchenkoio/main.py
+++ km94/tereshchenkoio/main.py
@@ -6,41 +6,46 @@ from bq_helper import BigQueryHelper
 import plotly.graph_objs as go
 from plotly.offline import plot
 
-bq_assistant = BigQueryHelper(,)
+bq_assistant = BigQueryHelper('bigquery-public-data','census_bureau_international')
 
 
 QUERY = """
-        SELECT  FROM
-        LIMIT 10000
+        SELECT country_code,country_name,year,midyear_population
+        FROM `bigquery-public-data.census_bureau_international.midyear_population`
+        LIMIT 30
         """
 
 
 df = bq_assistant.query_to_pandas(QUERY)
 
+country_name = df[df['country_name']=="Aruba"]
 
 
 
+"""trace1 = go.Scatter(
 
-trace1 = go.Scatter(
+
 
                     )
-
 
 
 trace2 = go.Pie(
 
-                    )
+                    )"""
 
 trace3 = go.Bar(
+                x = country_name['year'].values,
+                y = country_name['midyear_population'].values
+
 
 )
 
-data = [trace1]
+"""data = [trace1]"""
 
 layout = dict(
               title = '',
               xaxis= dict(title= ''),
               yaxis=dict(title=''),
              )
-fig = dict(data = [trace1], layout = layout)
+fig = dict(data = [trace3], layout = layout)
 plot(fig)
