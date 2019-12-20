d98901f93d32c51142c418bd4c396d6e3afe99ec
Romanov-Niko <kolya.romanov8@gmail.com>
2019-12-20 16:31:02 +0200

    scatter

--- km94/LashtabegAV/main.py.py
+++ km94/LashtabegAV/main.py.py
@@ -19,3 +19,10 @@ Aruba = df[df.country_name == 'Aruba']
 bar = plot.bar(Aruba, x = 'year', y = 'max_age',
                labels = {'year': 'Year', 'max_age': 'Max age'}, title = 'Aruba')
 bar.show()
+
+Year = df[df.year == 1981]
+scatter = plot.scatter(Year, x = 'sex', y = 'max_age',
+                       labels = {'year': 'Year', 'max_age': 'Max age'},
+                       text = 'country_name', title = 'Max age in 1981')
+scatter.update_traces(textposition = 'top senter')
+scatter.show()
