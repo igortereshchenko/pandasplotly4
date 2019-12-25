e16feac4b81d62b1eea7126732cbd4f698282bbb
bespalovad <55587308+bespalovad@users.noreply.github.com>
2019-12-25 14:56:27 +0300

    pie

--- km94/bespalovad/main.py
+++ km94/bespalovad/main.py
@@ -18,3 +18,7 @@ df = bq_assistant.query_to_pandas(QUERY)
 Aruba_ma_b = df[df.country_name == 'Aruba']
 bar = plot.bar(Aruba_ma_b, x = 'year', y = 'max_age', label = {'year': 'Year', 'max_age': 'Max_age'}, title = 'Aruba max age per year')
 bar.show()
+
+Aruba_s = df[df.year == 2001]
+pie = plot.pie(Aruba_s, x = 'year', y = 'sex', label = {'year': 'Year', 'sex': 'Sex'}, title = 'Aruba sex difference in 2001')
+pie.show()
